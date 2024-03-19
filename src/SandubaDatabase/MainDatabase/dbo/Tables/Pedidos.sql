92895f746c94984f1798072ce6233a5363e38e3f
cangelosilima <cangelosilima@gmail.com>
2024-03-18 22:58:41 -0300

    ci: remove project

--- src/SandubaDatabase/MainDatabase/dbo/Tables/Pedidos.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE Pedidos (
-    Id          UNIQUEIDENTIFIER    NOT NULL
-,   Numero      INT                 NOT NULL
-,   ClienteId   UNIQUEIDENTIFIER        NULL
-,   [Status]    INT                 NOT NULL DEFAULT (0)
-,   CONSTRAINT Pk_Pedidos PRIMARY KEY NONCLUSTERED (Id)
-,   CONSTRAINT Fk1_Pedidos FOREIGN KEY (ClienteId) REFERENCES Clientes (Id)
-)
