0357207f759036bd0551e5d9ced0492156f619dd
cangelosilima <cangelosilima@gmail.com>
2024-03-31 19:06:58 -0300

    feat: add random password and mongo db

--- azure/main.tf
+++ azure/main.tf
@@ -17,6 +17,20 @@ terraform {
 provider "azurerm" {
   features {}
 }
+
+resource "random_password" "mssql_user" {
+  length  = 10
+  lower   = true
+  upper   = true
+  special = false
+}
+
+resource "random_password" "mssql_password" {
+  length           = 16
+  special          = true
+  override_special = "!#$%&*()-_=+[]{}<>:?"
+}
+
 resource "azurerm_resource_group" "resource_group" {
   name       = "fiap-tech-challenge-database-group"
   location   = "eastus"
@@ -52,8 +66,8 @@ resource "azurerm_mssql_server" "sqlserver" {
   resource_group_name          = azurerm_resource_group.resource_group.name
   location                     = azurerm_resource_group.resource_group.location
   version                      = "12.0"
-  administrator_login          = var.main_sqlserver_adm_login
-  administrator_login_password = var.main_sqlserver_adm_password
+  administrator_login          = random_password.mssql_user.result
+  administrator_login_password = random_password.mssql_password.result
 
   tags = {
     environment = azurerm_resource_group.resource_group.tags["environment"]
@@ -84,14 +98,69 @@ resource "azurerm_mssql_database" "sanduba_main_database" {
   }
 }
 
-resource "github_actions_organization_secret" "fiap_main_database_connectionstring" {
-  secret_name     = "APP_MAIN_DATABASE_CONNECTION_STRING"
-  visibility      = "all"
-  plaintext_value = "Server=tcp:${azurerm_mssql_server.sqlserver.fully_qualified_domain_name},1433;Initial Catalog=${azurerm_mssql_database.sanduba_main_database.name};Persist Security Info=False;User ID=${var.main_sqlserver_adm_login};Password=${var.main_sqlserver_adm_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
+resource "azurerm_cosmosdb_account" "sanduba_payment_database_account" {
+  name                          = "sanduba-payment-database-account"
+  location                      = azurerm_resource_group.resource_group.location
+  resource_group_name           = azurerm_resource_group.resource_group.name
+  offer_type                    = "Standard"
+  enable_free_tier              = true
+  kind                          = "MongoDB"
+  public_network_access_enabled = true
+
+  enable_automatic_failover = false
+
+  mongo_server_version = 4.2
+
+  consistency_policy {
+    consistency_level = "BoundedStaleness"
+  }
+
+  geo_location {
+    location          = azurerm_resource_group.resource_group.location
+    failover_priority = 0
+  }
+
+  tags = {
+    environment = azurerm_resource_group.resource_group.tags["environment"]
+  }
 }
 
-resource "github_actions_organization_secret" "fiap_cart_database_connectionstring" {
+resource "azurerm_cosmosdb_mongo_database" "sanduba_payment_database" {
+  name                = "sanduba-payment-database"
+  resource_group_name = azurerm_cosmosdb_account.sanduba_payment_database_account.resource_group_name
+  account_name        = azurerm_cosmosdb_account.sanduba_payment_database_account.name
+}
+
+resource "azurerm_cosmosdb_mongo_collection" "sanduba_payment_database_collection" {
+  name                = "sanduba-payment-database-collection"
+  resource_group_name = azurerm_cosmosdb_mongo_database.sanduba_payment_database.resource_group_name
+  account_name        = azurerm_cosmosdb_mongo_database.sanduba_payment_database.account_name
+  database_name       = azurerm_cosmosdb_mongo_database.sanduba_payment_database.name
+
+  default_ttl_seconds = "777"
+  shard_key           = "uniqueKey"
+  throughput          = 400
+
+  index {
+    keys   = ["_id"]
+    unique = true
+  }
+}
+
+resource "github_actions_organization_secret" "main_database_connectionstring" {
+  secret_name     = "APP_MAIN_DATABASE_CONNECTION_STRING"
+  visibility      = "all"
+  plaintext_value = "Server=tcp:${azurerm_mssql_server.sqlserver.fully_qualified_domain_name},1433;Initial Catalog=${azurerm_mssql_database.sanduba_main_database.name};Persist Security Info=False;User ID=${random_password.mssql_user.result};Password=${random_password.mssql_password.result};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
+}
+
+resource "github_actions_organization_secret" "cart_database_connectionstring" {
   secret_name     = "APP_CART_DATABASE_CONNECTION_STRING"
   visibility      = "all"
   plaintext_value = azurerm_redis_cache.sanduba_cart_database.primary_connection_string
+}
+
+resource "github_actions_organization_secret" "payment_database_connectionstring" {
+  secret_name     = "APP_PAYMENT_DATABASE_CONNECTION_STRING"
+  visibility      = "all"
+  plaintext_value = azurerm_cosmosdb_account.sanduba_payment_database.primary_mongodb_connection_string 
 }
