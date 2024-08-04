9d88dad0a06bd648ea7a202daf4b5c677db092b0
cangelosilima <cangelosilima@gmail.com>
2024-08-03 23:33:19 -0300

    ci: add location and environment

--- variables.tf
+++ variables.tf
@@ -1,11 +1,20 @@
+variable "environment" {
+  type      = string
+  sensitive = false
+  default   = ""
+}
+
+variable "location" {
+  type      = string
+  sensitive = false
+  default   = ""
+}
+
 variable "authentication_secret_key" {
   type      = string
   sensitive = true
 }
 
-variable "environment" {
-  default = "Development"
-}
 
 variable "app_payment_url" {
   type      = string
